de051461ed9849bd4971a961e83c484bd08807a1
vishalvrma.404@gmail.com <vishalvrma.404@gmail.com>
2026-03-01 22:14:06 +0530

    Started Moodifi web app, first commit, initialized git repo and add face expression detecting using Google MediaPipe Face landmark library

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FaceExpression from './features/expression/components/FaceExpression';
+
+const App = () => {
+  return (
+   <FaceExpression />
+  )
+}
+
+export default App
